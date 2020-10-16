22ca106a3d257a8d5931a63ab4c5734733a733f1
Christos <christos@workable.com>
2020-10-16 10:49:07 +0300

    awesome click

--- sample/src/AwesomeWidget.js
+++ sample/src/AwesomeWidget.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import i18n from "shared/i18n";
 
 export default class AwesomeWidget extends React.Component {
@@ -9,15 +10,20 @@ export default class AwesomeWidget extends React.Component {
   }
 
   handleClick() {
-    //
+    this.prop.onClick();
   }
 
   render() {
     return (
       <div className="awesome_design_system_flex">
         <h2>{i18n.t("awesome_widget.header")}</h2>
-        <button onClick={this.handleClick}></button>
+        <button onClick={this.handleClick}>
+          {i18n.t("awesome_widget.primary_cta")}
+        </button>
       </div>
     );
   }
 }
+AwesomeWidget.PropTypes = {
+  onClick: PropTypes.func.isRequired,
+};
